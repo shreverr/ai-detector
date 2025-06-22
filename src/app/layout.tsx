d0ef5828d8d7f51f32133b9c40fafafdf7e430b9
Shreshth Verma <verma2007s@gmail.com>
2025-06-22 21:32:21 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics/> 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
